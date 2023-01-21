f811225ed63981d1f6de6f81b9b7e402364d269b
TimurMP <titimurmamyshev@gmail.commur>
2023-01-21 12:31:34 +0200

    Base

--- script.js
+++ script.js
@@ -1,137 +1,73 @@
-//id, firstName, lastName, age
-
-
+//id;firstName;lastName;age
 const persons = [];
-addp.addEventListener('click', addPersonOnButtonClick);
-showAll.addEventListener('click', e => printPersons(persons));
-stats.addEventListener('click', e => printStats(persons));
 
-
-// let inputData = prompt('Enter person data, separated by ","');
-
-//      55, 'Nathan', 'Drake', 30
-//      1, 'Nathan', 'Drake', 20
-//      2, 'Nathan', 'Drake', 21
-//      3, 'Nathan', 'Drake', 22
-//      4, 'Nathan', 'Drake', 23
-
-
-// while (inputData) {
-//     const pers = parsePersonInput(inputData);
-//     addPersonToArr(pers, persons)
-//     inputData = prompt('Enter person data, separated by ","');
-// }
-
-
-// printPersons(persons);
-// console.log("=============")
-// printStats(persons);
-
-
-function printStats(persons) {
-    const body = document.body;
-    const div = document.createElement("div");
-    div.style.border = '1px solid black';
-    div.style.backgroundColor = 'brown';
-
-    let arr = [];
-    persons.forEach((p) => arr.push(p.age));
-    let max = Math.max(...arr);
-    let min = Math.min(...arr);
-    const avg = arr.reduce((accum, item) => accum + item, 0) / arr.length;
-
-    const maxp = document.createElement("p");
-    maxp.append(`Max age is ${max}`);
-    div.appendChild(maxp);
-    body.appendChild(div);
-    const minp = document.createElement("p");
-    minp.append(`Min age is ${min}`);
-    div.appendChild(minp);
-    body.appendChild(div);
-    const avgp = document.createElement("p");
-    avgp.append(`Avg age is ${avg}`);
-    div.appendChild(avgp);
-    body.appendChild(div);
-    // console.log(`Max age is ${max}`);
-    // console.log(`Min age is ${min}`);
-    // console.log(`Avg age is ${avg}`);
-
-
+addPerson.onclick = function () {
+    const person = new Person(personId.value.trim(), firstName.value.trim(), lastName.value.trim(), age.value);
+    if (findPerson(persons, person.id) === -1) {
+        persons.push(person);
+    } else {
+        alert(`Person with id: ${person.id} exists`);
+    }
+    personId.value = firstName.value = lastName.value = age.value = '';
+}
+showPersons.onclick = function () {
+    printPersons(persons);
+}
+calcStats.onclick = function () {
+    printStats(persons);
 }
 
+function findPerson(persons, id) {
+    return persons.findIndex(p => p.id === id);
+}
 
 function printPersons(persons) {
-
-    const body = document.body;
-
-    const div = document.createElement("div");
-    div.style.border = '1px solid black';
-    div.style.backgroundColor = 'orange';
-    persons.forEach((p, i) => {
-        const pp = document.createElement("p");
-        pp.append(p.toString());
-        div.appendChild(pp);
+    while(personsList.firstElementChild){
+        personsList.removeChild(personsList.firstElementChild);
+    }
+    persons.forEach(p => {
+        const li = createInfoElement(p.toString(), 'li');
+        personsList.appendChild(li);
     });
-    body.appendChild(div);
 }
 
-// function parsePersonInput(inputData) {
-//     inputData.trim();
-//     const valArr = inputData.split(",");
-//     return new Person(valArr[0], valArr[1], valArr[2], +valArr[3]);
-// }
-
-function addPersonOnButtonClick() {
-    const pers = parsePersonInput();
-    addPersonToArr(pers, persons);
-    console.log(persons);
-    idInput.value = '';
-    nameInput.value = '';
-    lastNameInput.value = '';
-    ageInput.value = '';
-
-}
-
-function parsePersonInput() {
-    // const valArr = [idInput.value, nameInput.value, lastNameInput.value, ageInput.value];
-    // const persons = [];
-    // valArr.push(idInput.value);
-    // console.log('---------')
-    // console.log(valArr);
-    // for (let i = 0; i < people.children.length; i++) {
-    //     valArr.push(people.children[i]);
-    // }
-    // valArr.forEach(n => {
-    //     persons.push(n.value);
-    // })
-    // console.log(persons);
-    return new Person(idInput.value, nameInput.value, lastNameInput.value, +ageInput.value);
-}
-
-
-function addPersonToArr(person, arr) {
-    let res = arr.findIndex(p => p.id === person.id)
-    console.log(res)
-    if (res !== -1) {
-        alert(`Person with ID ${person.id} already exists!`)
+function printStats(persons) {
+    if (persons.length) {
+        const start = persons[0].age
+        const minAge = persons.reduce((res, p) => p.age < res ? p.age : res, start);
+        const maxAge = persons.reduce((res, p) => p.age > res ? p.age : res, start);
+        const avgAge = persons.reduce((res, p) => p.age + res, 0) / persons.length;
+        const divStats = document.createElement('div')
+        const h3avg = createInfoElement(`Average age: ${avgAge.toFixed(1)}`, 'h3');
+        const h3min = createInfoElement(`Min age: ${minAge}`, 'h3');
+        const h3max = createInfoElement(`Max age: ${maxAge}`, 'h3');
+        divStats.append(h3avg, h3min, h3max);
+        if (stats.firstElementChild.nextElementSibling) {
+            stats.replaceChild(divStats, stats.firstElementChild.nextElementSibling);
+        } else {
+            stats.appendChild(divStats);
+        }
     } else {
-        arr.push(person);
+        stats.appendChild(createInfoElement('No stats', 'h3'));
     }
 }
-
 
 function Person(id, firstName, lastName, age) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
-    this.age = age;
+    this.age = +age;
     this.fullName = function () {
-        return `${this.firstName} ${this.lastName}`;
+        return `${this.firstName} ${this.lastName}`
     }
     this.toString = function () {
         return `ID: ${this.id}, Name: ${this.fullName()}, Age: ${this.age}`
     }
-
-
 }
 
+function createInfoElement(content, tag) {
+    const element = document.createElement(tag);
+    const text = document.createTextNode(content);
+    element.appendChild(text);
+    return element;
+}
